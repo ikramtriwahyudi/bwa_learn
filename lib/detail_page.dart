5eabcf1ed06c4016e1584146523027ad5d74278d
ikramee <ikramtriwahyudi@gmail.com>
2024-07-04 15:50:45 +0800

    update

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -21,6 +21,106 @@ class _Detail_pageState extends State<Detail_page> {
               height: 350,
               fit: BoxFit.cover,
             ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      "assets/btn_back.png",
+                      width: 40,
+                    ),
+                  ),
+                  Image.asset(
+                    "assets/btn_wishlist.png",
+                    width: 40,
+                  ),
+                ],
+              ),
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 35,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Kuretakeso Hott",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                      Text("\$52 / month"),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("Main Facilities"),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "assets/dtlicon1.png",
+                                width: 32,
+                                height: 32,
+                              ),
+                              SizedBox(height: 8),
+                              Text("2 kitchen")
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "assets/dtlicon2.png",
+                                width: 32,
+                                height: 32,
+                              ),
+                              SizedBox(height: 8),
+                              Text("3 bedroom")
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "assets/dtlicon3.png",
+                                width: 32,
+                                height: 32,
+                              ),
+                              SizedBox(height: 8),
+                              Text("3 big lemari")
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
